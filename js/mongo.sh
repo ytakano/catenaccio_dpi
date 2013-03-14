0dc501968994b4747e8dd9a7ba5729933b88787e
Yuuki Takano <ytakanoster@gmail.com>
2013-03-15 01:54:04 +0900

    fixed shell script

--- js/mongo.sh
+++ js/mongo.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-mongo HTTP mongo_http_hosts.js
-mongo HTTP mongo_http_graph_uri.js
-mongo HTTP mongo_http_graph_host.js
-mongo HTTP mongo_http_graph_trunc_host.js
-mongo HTTP mongo_http_tld.js
+js_files="mongo_http_hosts.js mongo_http_graph_uri.js mongo_http_graph_host.js mongo_http_graph_trunc_host.js mongo_http_tld.js"
+
+js_path=`dirname $0`
+
+if [ $# -ge 1 ]; then
+    host=$1/HTTP
+else
+    host=HTTP
+fi
+
+for i in ${js_files}
+do
+    echo "${host} ${js_path}/${i}"
+    mongo ${host} ${js_path}/${i}
+done
