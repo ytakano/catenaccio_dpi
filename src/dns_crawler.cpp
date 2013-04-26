183e28a433a732db2cf089bb74d96ab7ad334e72
Yuuki Takano <ytakanoster@gmail.com>
2013-04-26 10:42:52 +0900

    fixed a bug

--- src/dns_crawler.cpp
+++ src/dns_crawler.cpp
@@ -118,33 +118,6 @@ init_arr(uint8_t *arr)
     }
 }
 
-/*
-bool
-send_query(string server, int fd, cb_arg *arg)
-{
-    int s;
-
-    if (arg == NULL)
-        arg = new cb_arg(fd);
-
-    memset(&arg->m_saddr, 0, sizeof(arg->m_saddr));
-
-    s = inet_pton(AF_INET, server.c_str(), &arg->m_saddr.sin_addr);
-    if (s < 0) {
-        perror("inet_pton");
-        return false;
-    }
-
-    arg->m_saddr.sin_port   = htons(53);
-    arg->m_saddr.sin_family = AF_INET;
-
-    sendto(fd, query, sizeof(query), 0,
-           (sockaddr*)&arg->m_saddr, sizeof(arg->m_saddr));
-
-    return true;
-}
-*/
-
 void
 send_query(evutil_socket_t fd, short what, void *arg)
 {
@@ -252,7 +225,7 @@ callback_dns(evutil_socket_t fd, short what, void *arg)
 
                     p_txt = DNS_RDATA_TO_TXT(it->m_rdata);
 
-                    b.append("_id", p_txt->m_txt);
+                    b.append("ver", p_txt->m_txt);
 
                     break;
                 }
@@ -270,84 +243,6 @@ callback_dns(evutil_socket_t fd, short what, void *arg)
     default:
         ;
     }
-/*
-    cb_arg *carg = (cb_arg*)arg;
-
-    switch (what) {
-    case EV_READ:
-    {
-        char buf[1024];
-        sockaddr_in saddr;
-        socklen_t   slen = sizeof(saddr);
-        ssize_t readlen;
-
-        readlen = recvfrom(fd, buf, sizeof(buf), 0, (sockaddr*)&saddr, &slen);
-
-        if (readlen < 0)
-            break;
-
-        if (carg->m_saddr.sin_addr.s_addr == saddr.sin_addr.s_addr &&
-            carg->m_saddr.sin_port == saddr.sin_port) {
-            char addr[128];
-
-            inet_ntop(AF_INET, &saddr.sin_addr, addr, sizeof(addr));
-
-            cout << "reply from " << addr << ":"
-                 << ntohs(saddr.sin_port) << endl;
-
-            cdpi_dns dns;
-
-            if (dns.decode(buf, readlen)) {
-                const list<cdpi_dns_rr> &ans = dns.get_answer();
-                list<cdpi_dns_rr>::const_iterator it;
-
-                for (it = ans.begin(); it != ans.end(); ++it) {
-                    if (ntohs(it->m_type) == DNS_TYPE_TXT &&
-                        ntohs(it->m_class) == DNS_CLASS_CH) {
-                        mongo::BSONObjBuilder b1, b2, b3;
-                        mongo::BSONObj        doc1, doc2;
-                        ptr_cdpi_dns_txt      p_txt;
-
-                        p_txt = DNS_RDATA_TO_TXT(it->m_rdata);
-
-                        b1.append("_id", addr);
-                        b2.append("ver", p_txt->m_txt);
-                        b3.append("$set", b2.obj());
-
-                        doc1 = b1.obj();
-                        doc2 = b3.obj();
-
-                        mongo_conn.update("DNS.servers", doc1, doc2);
-
-                        cout << p_txt->m_txt << endl;
-
-                        break;
-                    }
-                }
-            }
-        } else {
-            return;
-        }
-        break;
-    }
-    default:
-        ;
-    }
-
-    while (dns_servers.size() > 0) {
-        if (send_query(*dns_servers.begin(), fd, carg)) {
-            dns_servers.erase(dns_servers.begin());
-            break;
-        } else {
-            dns_servers.erase(dns_servers.begin());
-        }
-    }
-
-    if (dns_servers.size() <= 0) {
-        event_del(carg->m_ev);
-        event_free(carg->m_ev);
-    }
-*/
 }
 
 void
@@ -392,31 +287,6 @@ void
 get_dns_ver()
 {
     init();
-/*
-    int i = 0;
-    for (set<string>::iterator it = dns_servers.begin();
-         it != dns_servers.end();) {
-        int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-        if (fd < 0) {
-            perror("socket");
-            exit(-1);
-        }
-
-        cout << *it << endl;
-        
-        if (! send_query(*it, fd, NULL)) {
-            dns_servers.erase(it++);
-            continue;
-        }
-
-        dns_servers.erase(it++);
-        i++;
-
-        if (i >= MAX_UDP_SOCK)
-            break;
-    }
-*/
 
     event_base_dispatch(ev_base);
 }
