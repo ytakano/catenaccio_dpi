3ed97dada1ff5ed2dec14dcefc6897740024e0c8
Yuuki Takano <ytakanoster@gmail.com>
2013-05-17 17:18:40 +0900

    added files to analyzing DNS servers

--- py/dns_hist_single.py
+++ py/dns_hist_single.py
@@ -10,54 +10,37 @@ con   = pymongo.Connection(mongo)
 
 db = con.DNSCrawl
 
-max_n = 25
+max_n = 29
 width = 1.0
 data  = {}
 x     = []
 n     = 0
-xmin  = 10
-xmax  = 1000000
+xmin  = 1
+xmax  = 10000
 
-plt.figure(figsize=(12, 6))
-
-plt.axes([0.12, 0.1, 0.35, 0.8])
+plt.figure(figsize=(6, 8))
 
 plt.xlim(xmin=xmin, xmax=xmax)
+plt.ylim(ymin=0, ymax=max_n * width)
 plt.xscale('log')
 plt.xlabel('#address')
 
 for i in db.type_hist_unbound.find():
-    if i['_id'] == '':
+    if i['_id'] == None:
         continue
     data[i['_id']] = i['value']
 
 for k, v in sorted(data.items(), key = lambda x: x[1], reverse = True):
     x.insert(0, k)
-
-    if n >= max_n:
-        n2 = n - max_n
-    else:
-        n2 = n
-
-    plt.barh(max_n * width - (n2 + 1)* width, v, width,
+    plt.barh(max_n * width - (n + 1)* width, v, width,
              color = 'c', edgecolor = 'k')
     n += 1
 
     print k, v
 
     if n == max_n:
-        plt.yticks(numpy.arange(len(x)) + width / 2, x)
-        #break
-
-        plt.axes([0.6, 0.1, 0.35, 0.8])
-        plt.xlim(xmin=xmin, xmax=xmax)
-        plt.xscale('log')
-        plt.xlabel('#address')
-
-        x = []
-
-    if n >= max_n * 2:
-        plt.yticks(numpy.arange(len(x)) + width / 2, x)
         break
+
+plt.yticks(numpy.arange(len(x)) + width / 2, x)
 
 plt.show()
