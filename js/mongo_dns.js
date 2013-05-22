4f1aba078251fe5c9751b34f01ee74917f7eb1b6
Yuuki Takano <ytakanoster@gmail.com>
2013-05-23 03:53:17 +0900

    added and updated files for DNS

---
+++ js/mongo_dns.js
@@ -0,0 +1,393 @@
+var rirmap = {
+    '0': 'IANA - Local Identification',
+    '1': 'APNIC',
+    '2': 'RIPE NCC',
+    '3': 'General Electric Company',
+    '4': 'Level 3 Communications, Inc.',
+    '5': 'RIPE NCC',
+    '6': 'Army Information Systems Center',
+    '7': 'Administered by ARIN',
+    '8': 'Level 3 Communications, Inc.',
+    '9': 'IBM',
+    '10': 'IANA - Private Use',
+    '11': 'DoD Intel Information Systems',
+    '12': 'AT&T Bell Laboratories',
+    '13': 'Xerox Corporation',
+    '14': 'APNIC',
+    '15': 'Hewlett-Packard Company',
+    '16': 'Digital Equipment Corporation',
+    '17': 'Apple Computer Inc.',
+    '18': 'MIT',
+    '19': 'Ford Motor Company',
+    '20': 'Computer Sciences Corporation',
+    '21': 'DDN-RVN',
+    '22': 'Defense Information Systems Agency',
+    '23': 'ARIN',
+    '24': 'ARIN',
+    '25': 'UK Ministry of Defence',
+    '26': 'Defense Information Systems Agency',
+    '27': 'APNIC',
+    '28': 'DSI-North',
+    '29': 'Defense Information Systems Agency',
+    '30': 'Defense Information Systems Agency',
+    '31': 'RIPE NCC',
+    '32': 'AT&T Global Network Services',
+    '33': 'DLA Systems Automation Center',
+    '34': 'Halliburton Company',
+    '35': 'Administered by ARIN',
+    '36': 'APNIC',
+    '37': 'RIPE NCC',
+    '38': 'PSINet, Inc.',
+    '39': 'APNIC',
+    '40': 'Administered by ARIN',
+    '41': 'AFRINIC',
+    '42': 'APNIC',
+    '43': 'Administered by APNIC',
+    '44': 'Amateur Radio Digital Communications',
+    '45': 'Administered by ARIN',
+    '46': 'RIPE NCC',
+    '47': 'Administered by ARIN',
+    '48': 'Prudential Securities Inc.',
+    '49': 'APNIC',
+    '50': 'ARIN',
+    '51': 'UK Government Department for Work and Pensions',
+    '52': 'E.I. duPont de Nemours and Co., Inc.',
+    '53': 'Cap Debis CCS',
+    '54': 'Administered by ARIN',
+    '55': 'DoD Network Information Center',
+    '56': 'US Postal Service',
+    '57': 'SITA',
+    '58': 'APNIC',
+    '59': 'APNIC',
+    '60': 'APNIC',
+    '61': 'APNIC',
+    '62': 'RIPE NCC',
+    '63': 'ARIN',
+    '64': 'ARIN',
+    '65': 'ARIN',
+    '66': 'ARIN',
+    '67': 'ARIN',
+    '68': 'ARIN',
+    '69': 'ARIN',
+    '70': 'ARIN',
+    '71': 'ARIN',
+    '72': 'ARIN',
+    '73': 'ARIN',
+    '74': 'ARIN',
+    '75': 'ARIN',
+    '76': 'ARIN',
+    '77': 'RIPE NCC',
+    '78': 'RIPE NCC',
+    '79': 'RIPE NCC',
+    '80': 'RIPE NCC',
+    '81': 'RIPE NCC',
+    '82': 'RIPE NCC',
+    '83': 'RIPE NCC',
+    '84': 'RIPE NCC',
+    '85': 'RIPE NCC',
+    '86': 'RIPE NCC',
+    '87': 'RIPE NCC',
+    '88': 'RIPE NCC',
+    '89': 'RIPE NCC',
+    '90': 'RIPE NCC',
+    '91': 'RIPE NCC',
+    '92': 'RIPE NCC',
+    '93': 'RIPE NCC',
+    '94': 'RIPE NCC',
+    '95': 'RIPE NCC',
+    '96': 'ARIN',
+    '97': 'ARIN',
+    '98': 'ARIN',
+    '99': 'ARIN',
+    '100': 'ARIN',
+    '101': 'APNIC',
+    '102': 'AFRINIC',
+    '103': 'APNIC',
+    '104': 'ARIN',
+    '105': 'AFRINIC',
+    '106': 'APNIC',
+    '107': 'ARIN',
+    '108': 'ARIN',
+    '109': 'RIPE NCC',
+    '110': 'APNIC',
+    '111': 'APNIC',
+    '112': 'APNIC',
+    '113': 'APNIC',
+    '114': 'APNIC',
+    '115': 'APNIC',
+    '116': 'APNIC',
+    '117': 'APNIC',
+    '118': 'APNIC',
+    '119': 'APNIC',
+    '120': 'APNIC',
+    '121': 'APNIC',
+    '122': 'APNIC',
+    '123': 'APNIC',
+    '124': 'APNIC',
+    '125': 'APNIC',
+    '126': 'APNIC',
+    '127': 'IANA - Loopback',
+    '128': 'Administered by ARIN',
+    '129': 'Administered by ARIN',
+    '130': 'Administered by ARIN',
+    '131': 'Administered by ARIN',
+    '132': 'Administered by ARIN',
+    '133': 'Administered by APNIC',
+    '134': 'Administered by ARIN',
+    '135': 'Administered by ARIN',
+    '136': 'Administered by ARIN',
+    '137': 'Administered by ARIN',
+    '138': 'Administered by ARIN',
+    '139': 'Administered by ARIN',
+    '140': 'Administered by ARIN',
+    '141': 'Administered by RIPE NCC',
+    '142': 'Administered by ARIN',
+    '143': 'Administered by ARIN',
+    '144': 'Administered by ARIN',
+    '145': 'Administered by RIPE NCC',
+    '146': 'Administered by ARIN',
+    '147': 'Administered by ARIN',
+    '148': 'Administered by ARIN',
+    '149': 'Administered by ARIN',
+    '150': 'Administered by APNIC',
+    '151': 'Administered by RIPE NCC',
+    '152': 'Administered by ARIN',
+    '153': 'Administered by APNIC',
+    '154': 'Administered by AFRINIC',
+    '155': 'Administered by ARIN',
+    '156': 'Administered by ARIN',
+    '157': 'Administered by ARIN',
+    '158': 'Administered by ARIN',
+    '159': 'Administered by ARIN',
+    '160': 'Administered by ARIN',
+    '161': 'Administered by ARIN',
+    '162': 'Administered by ARIN',
+    '163': 'Administered by APNIC',
+    '164': 'Administered by ARIN',
+    '165': 'Administered by ARIN',
+    '166': 'Administered by ARIN',
+    '167': 'Administered by ARIN',
+    '168': 'Administered by ARIN',
+    '169': 'Administered by ARIN',
+    '170': 'Administered by ARIN',
+    '171': 'Administered by APNIC',
+    '172': 'Administered by ARIN',
+    '173': 'ARIN',
+    '174': 'ARIN',
+    '175': 'APNIC',
+    '176': 'RIPE NCC',
+    '177': 'LACNIC',
+    '178': 'RIPE NCC',
+    '179': 'LACNIC',
+    '180': 'APNIC',
+    '181': 'LACNIC',
+    '182': 'APNIC',
+    '183': 'APNIC',
+    '184': 'ARIN',
+    '185': 'RIPE NCC',
+    '186': 'LACNIC',
+    '187': 'LACNIC',
+    '188': 'Administered by RIPE NCC',
+    '189': 'LACNIC',
+    '190': 'LACNIC',
+    '191': 'Administered by LACNIC',
+    '192': 'Administered by ARIN',
+    '193': 'RIPE NCC',
+    '194': 'RIPE NCC',
+    '195': 'RIPE NCC',
+    '196': 'Administered by AFRINIC',
+    '197': 'AFRINIC',
+    '198': 'Administered by ARIN',
+    '199': 'ARIN',
+    '200': 'LACNIC',
+    '201': 'LACNIC',
+    '202': 'APNIC',
+    '203': 'APNIC',
+    '204': 'ARIN',
+    '205': 'ARIN',
+    '206': 'ARIN',
+    '207': 'ARIN',
+    '208': 'ARIN',
+    '209': 'ARIN',
+    '210': 'APNIC',
+    '211': 'APNIC',
+    '212': 'RIPE NCC',
+    '213': 'RIPE NCC',
+    '214': 'US-DOD',
+    '215': 'US-DOD',
+    '216': 'ARIN',
+    '217': 'RIPE NCC',
+    '218': 'APNIC',
+    '219': 'APNIC',
+    '220': 'APNIC',
+    '221': 'APNIC',
+    '222': 'APNIC',
+    '223': 'APNIC',
+    '224': 'Multicast',
+    '225': 'Multicast',
+    '226': 'Multicast',
+    '227': 'Multicast',
+    '228': 'Multicast',
+    '229': 'Multicast',
+    '230': 'Multicast',
+    '231': 'Multicast',
+    '232': 'Multicast',
+    '233': 'Multicast',
+    '234': 'Multicast',
+    '235': 'Multicast',
+    '236': 'Multicast',
+    '237': 'Multicast',
+    '238': 'Multicast',
+    '239': 'Multicast',
+    '240': 'Future use',
+    '241': 'Future use',
+    '242': 'Future use',
+    '243': 'Future use',
+    '244': 'Future use',
+    '245': 'Future use',
+    '246': 'Future use',
+    '247': 'Future use',
+    '248': 'Future use',
+    '249': 'Future use',
+    '250': 'Future use',
+    '251': 'Future use',
+    '252': 'Future use',
+    '253': 'Future use',
+    '254': 'Future use',
+    '255': 'Future use'
+};
+
+db.servers.find().forEach(function(doc) {
+    var ver;
+
+    try {
+        if (doc['ver'].match(/^dnsmasq-/)) {
+            // DNS masquerade
+            ver = doc['ver'].split('-')[1];
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'dnsmasq', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^Nominum Vantio /)) {
+            // Nominum Vantio
+            ver = doc['ver'].split(' ')[2];
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'Nominum Vantio', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^Nominum ANS /)) {
+            // Nominum ANS
+            ver = doc['ver'].split(' ')[2];
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'Nominum ANS', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^unbound/)) {
+            // unbound
+            ver = doc['ver'].split(' ')[1];
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'unbound', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^NSD/)) {
+            // NSD
+            ver = doc['ver'].split(' ')[1];
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'NSD', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/.*Windows/)) {
+            // Windows
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'Windows'}}
+            );
+        } else if (doc['ver'].match(/^PowerDNS/)) {
+            // PowerDNS
+            ver = doc['ver'].split(' ')[2];
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'PowerDNS', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^4\.[0-9]\.[0-9]/)) {
+            // BIND 4.x
+            ver = get_type_ver_bind48(doc['ver']);
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'BIND 4.x', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^8\.[0-9]\.[0-9]/)) {
+            // BIND 8.x
+            ver = get_type_ver_bind48(doc['ver']);
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'BIND 8.x', type_ver: ver}}
+            );
+        } else if (doc['ver'].match(/^9\.[0-9]\.[0-9]/)) {
+            // BIND 9.x
+            ver = get_type_ver_bind9(doc['ver']);
+
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {type: 'BIND 9.x', type_ver: ver}}
+            );
+        }
+
+        var prefix = doc['_id'].split('.')[0];
+        if (prefix in rirmap) {
+            db.servers.update(
+                {_id: doc['_id']},
+                {$set: {rir: rirmap[prefix]}}
+            );
+        }
+    } catch (e) {
+    }
+});
+
+function get_type_ver_bind48(ver) {
+    var sp       = ver.split(/-| |\+/);
+    var type_ver = sp[0];
+
+    if (sp.length >= 2 && sp[1].match(/^P[1-9]+$/)) {
+        type_ver += '-' + sp[1];
+    }
+
+    if (ver.match(/.*REL/)) {
+        ver += '-REL';
+    }
+
+    return type_ver;
+}
+
+function get_type_ver_bind9(ver) {
+    var sp       = ver.split(/-| |\+/);
+    var type_ver = sp[0];
+
+    if (sp.length >= 2 && sp[1].match(/^P[1-9][0-9]*$/)) {
+        type_ver += '-' + sp[1];
+    } else if (sp.length >= 2 && sp[1].match(/^ESV$/)) {
+        type_ver += '-' + sp[1];
+        if (sp.length >= 3 && sp[2].match(/^R[1-9][0-9]*$/)) {
+            type_ver += '-' + sp[2];
+        } else if (sp.length >= 4 && sp[3].match(/^P[1-9][0-9]*$/)) {
+            type_ver += '-' + sp[3];
+        }
+    } else if (sp.length >= 3 && sp[2].match(/^P[1-9]+$/)) {
+        type_ver += '-' + sp[2];
+    }
+
+    if (ver.match(/.*RedHat/)) {
+        ver += '(RedHat)';
+    }
+
+    return type_ver;
+}
