6d393983c7da4448934345e6e851a6191bebcff0
Yuuki Takano <ytakanoster@gmail.com>
2013-08-23 14:07:34 +0900

    update

--- py/index.py
+++ py/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 def application(environ, start_response):
 	start_response('200 OK', [('Content-type', 'text/html')])
-	return "<html><head><title>Test</title></head><body><div>Hello</div></body></hetml>"
+	return "<html><head><title>Test</title></head><body>AAA" + os.getcwd() + "<div>Hello</div></body></hetml>"
